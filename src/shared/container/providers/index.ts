36c24ba7ebecd6090c524c42747a9ffec2a05d32
Ana Alves <ramos.alvesac@gmail.com>
2023-07-03 22:13:20 -0300

    Added bcrypt and uuid dependencie inverse

---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,10 @@
+import { container } from "tsyringe";
+
+import { IUuidProvider } from "./uuidProvider/IUuidProvider";
+import { UuidProvider } from "./uuidProvider/implementation/UuidProvider";
+import { IBcryptProvider } from "./bcryptProvider/IBcryptProvider";
+import { BcryptProvider } from "./bcryptProvider/implementation/BcryptProvider";
+
+container.registerSingleton<IUuidProvider>("UuidProvider", UuidProvider);
+
+container.registerSingleton<IBcryptProvider>("BcryptProvider", BcryptProvider);
---
+++ src/shared/container/providers/uuidProvider/IUuidProvider.ts
@@ -0,0 +1,5 @@
+interface IUuidProvider {
+  createUUID(): string;
+  validateUUID(uuid: string): boolean;
+}
+export { IUuidProvider };
