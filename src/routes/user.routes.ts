97d9baf6d653798e8075d905fad4155a7256c713
Ana Alves <ramos.alvesac@gmail.com>
2023-07-04 21:56:10 -0300

    Added updateUser

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController";
+import { UpdateUserController } from "@modules/users/useCases/updateUser/updateUserController";
 import { Router } from "express";
 
 const userRoutes = Router();
 
 userRoutes.post("/", new CreateUserController().handle);
+userRoutes.put("/:id", new UpdateUserController().handle);
 
 export { userRoutes };
--- src/utils/formatData.ts
+++ src/utils/formatData.ts
@@ -1,11 +1,11 @@
-function telephoneFormat(telephone: string | null): string | null {
+function telephoneFormat(telephone: string | null): string | undefined {
   return telephone
     ? telephone // (17) 98803-9217
         .replaceAll("(", "") // (17 98803-9217
         .replaceAll(")", "") //
         .replaceAll(" ", "")
         .replaceAll("-", "")
-    : null;
+    : undefined;
 }
 
 export { telephoneFormat };
