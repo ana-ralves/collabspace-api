deec8877608e27bdf6d3141b30de6ee50df239d1
Ana Alves <ramos.alvesac@gmail.com>
2023-07-07 20:15:46 -0300

    fixbug

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,18 @@
-import { CreateUserController } from "@modules/users/useCases/createUser/createUserController";
-import { InactivateUserController } from "@modules/users/useCases/inactivateUser/inactivateUserController";
-import { UpdateUserController } from "@modules/users/useCases/updateUser/updateUserController";
 import { Router } from "express";
+
+import { CreateUserController } from "@modules/users/useCases/createUser/createUserController";
+import { UpdateUserController } from "@modules/users/useCases/updateUser/updateUserController";
+import { InactivateUserController } from "@modules/users/useCases/inactivateUser/inactivateUserController";
+
+import { authentication } from "src/middlewares/authentication";
 
 const userRoutes = Router();
 
 userRoutes.post("/", new CreateUserController().handle);
-userRoutes.put("/:id", new UpdateUserController().handle);
-userRoutes.delete("/:id", new InactivateUserController().handle);
+
+userRoutes.use(authentication);
+
+userRoutes.put("/", new UpdateUserController().handle);
+userRoutes.delete("/", new InactivateUserController().handle);
 
 export { userRoutes };
